4752596061a5e73b68fc9e6187a08bae4b979fd0
zhongbin <18370411824@163.com>
2021-03-09 17:47:46 +0800

    插槽的用法

---
+++ src/components/SlotChild.vue
@@ -0,0 +1,49 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-22 10:51:34
+ * @LastEditTime: 2020-12-22 11:23:59
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \vue-slot-demo\src\components\SlotChild.vue
+-->
+<template>
+  <div class="slot-child">
+    <header>
+      <slot name="header" :userData="userInfo">header</slot>
+    </header>
+    <main>
+      <slot :userData="userInfo">default</slot>
+    </main>
+    <footer>
+      <slot name="footer" :userData="userInfo">footer</slot>
+    </footer>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'SlotChild',
+  data () {
+    return {
+      userInfo: {
+        name: '张三',
+        age: '18',
+        say: 'Hello World'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.slot-child {
+  header,
+  main,
+  footer {
+    box-sizing: border-box;
+    border-bottom: 1px solid #ccc;
+    padding: 10px 0;
+  }
+}
+</style>
